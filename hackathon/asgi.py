223aadff4c27573ca23018689f5f67cd3efc4d85
Swastika <swastika@Swastikas-MacBook-Air.local>
2025-11-28 00:35:21 +0530

    Render deployment fixes

--- hackathon/asgi.py
+++ hackathon/asgi.py
@@ -2,14 +2,16 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings')
 
-django_asgi_app = get_asgi_application()
+django.setup()   # IMPORTANT — load apps BEFORE import routing
+
+import chat.routing  # now safe
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
