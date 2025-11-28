7a778178f7d306ca6575e5e26fa5c13bee2da43c
Swastika <swastika@Swastikas-MacBook-Air.local>
2025-11-28 12:13:41 +0530

    Fix ASGI loading order for Django Channels

--- hackathon/asgi.py
+++ hackathon/asgi.py
@@ -2,17 +2,23 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings')
+
+# Load Django first
+django_asgi_app = get_asgi_application()
+
+# Import routing AFTER Django is ready
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syncroomies.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
 })
-
 
 
 # import os
