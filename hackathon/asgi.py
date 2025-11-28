9db44eec4938f64af17051f6b4c7d02763184976
Swastika <swastika@Swastikas-MacBook-Air.local>
2025-11-28 10:28:09 +0530

    Update chat frontend WebSocket URL

--- hackathon/asgi.py
+++ hackathon/asgi.py
@@ -2,19 +2,36 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import django
+import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings')
-
-django.setup()   # IMPORTANT — load apps BEFORE import routing
-
-import chat.routing  # now safe
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syncroomies.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter(chat.routing.websocket_urlpatterns)
     ),
 })
+
+
+
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# import django
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings')
+
+# django.setup()   # IMPORTANT — load apps BEFORE import routing
+
+# import chat.routing  # now safe
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
